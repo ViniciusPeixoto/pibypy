5022fdcbc6e916d9823249de28227a223e6dcfb2
Vinicius Peixoto <vinicius.opeixoto@gmail.com>
2024-03-16 14:00:41 -0300

    Addition, subtraction and division algorithms plus tests

--- tests/unit_tests/utils/test_big_math.py
+++ tests/unit_tests/utils/test_big_math.py
@@ -4,12 +4,41 @@ from utils.big_math import (
     _fits,
     _calculate_division,
     division,
-    DECIMAL_LIMIT,
+    _match_number_sizes,
+    _add_string,
+    add,
+    _sub_string,
+    sub,
+    Decimal,
 )
 
+
 @pytest.mark.parametrize(
-        "numerator, denominator, expected",
-        [("3", "2", True), ("2", "3", False),]
+    "params, expected",
+    [
+        ({}, Decimal()),
+        ({"number": ""}, Decimal(number="0.0")),
+        ({"whole": ""}, Decimal(number="0.0")),
+        ({"decimal": ""}, Decimal(number="0.0")),
+        ({"is_negative": True}, Decimal(number="0.0", is_negative=False)),
+        ({"number": "3.14"}, Decimal(whole="3", decimal="14", is_negative=False)),
+        ({"number": "-3.14"}, Decimal(whole="3", decimal="14", is_negative=True)),
+        ({"number": "314"}, Decimal(whole="314", decimal="0", is_negative=False)),
+        ({"number": ".56"}, Decimal(whole="0", decimal="56", is_negative=False)),
+    ],
+)
+def test_decimal(params, expected):
+    result = Decimal(**params)
+
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "numerator, denominator, expected",
+    [
+        ("3", "2", True),
+        ("2", "3", False),
+    ],
 )
 def test_fits(numerator, denominator, expected):
     result = _fits(numerator, denominator)
@@ -18,13 +47,13 @@ def test_fits(numerator, denominator, expected):
 
 
 @pytest.mark.parametrize(
-        "numerator, denominator, expected",
-        [
-            ("3", "2", ("1", "1")),
-            ("2", "3", ("0", "2")),
-            ("4", "4", ("1", "0")),
-            ("4", "2", ("2", "0")),
-        ]
+    "numerator, denominator, expected",
+    [
+        ("3", "2", ("1", "1")),
+        ("2", "3", ("0", "2")),
+        ("4", "4", ("1", "0")),
+        ("4", "2", ("2", "0")),
+    ],
 )
 def test_calculate_division(numerator, denominator, expected):
     result = _calculate_division(numerator, denominator)
@@ -33,15 +62,201 @@ def test_calculate_division(numerator, denominator, expected):
 
 
 @pytest.mark.parametrize(
-        "numerator, denominator, fixed, repeating_decimal",
-        [
-            ("3", "2", "1.5", ""),
-            ("1", "3", "0.", "3"),
-            ("1", "7", "0.", "142857"),
-        ],
-        indirect=["repeating_decimal"]
+    "numerator, denominator, fixed, repeating_decimal",
+    [
+        (Decimal(number="3.0"), Decimal(number="2.0"), "1.5", ""),
+        (Decimal(number="1.0"), Decimal(number="3.0"), "0.", "3"),
+        (Decimal(number="1.0"), Decimal(number="7.0"), "0.", "142857"),
+        (Decimal(number="-3.0"), Decimal(number="2.0"), "-1.5", ""),
+        (Decimal(number="1.0"), Decimal(number="-3.0"), "-0.", "3"),
+        (Decimal(number="-1.0"), Decimal(number="-7.0"), "0.", "142857"),
+    ],
+    indirect=["repeating_decimal"],
 )
 def test_division(numerator, denominator, fixed, repeating_decimal):
     result = division(numerator, denominator)
 
     assert result == fixed + repeating_decimal
+
+
+@pytest.mark.parametrize(
+    "number_1, number_2, expected",
+    [
+        (
+            Decimal(number="33.3"),
+            Decimal(number="2.222"),
+            (Decimal(number="33.300"), Decimal(number="02.222")),
+        ),
+        (
+            Decimal(number="3333.333333"),
+            Decimal(number="0.0"),
+            (Decimal(number="3333.333333"), Decimal(number="0000.000000")),
+        ),
+    ],
+)
+def test_match_number_sizes(number_1, number_2, expected):
+    _match_number_sizes(number_1, number_2)
+
+    assert (number_1, number_2) == expected
+
+
+@pytest.mark.parametrize(
+    "digit_1, digit_2, carry, expected",
+    [
+        (
+            "2",
+            "3",
+            "0",
+            ("0", "5"),
+        ),
+        (
+            "2",
+            "3",
+            "3",
+            ("0", "8"),
+        ),
+        (
+            "2",
+            "3",
+            "4",
+            ("0", "9"),
+        ),
+        (
+            "2",
+            "3",
+            "6",
+            ("1", "1"),
+        ),
+        (
+            "9",
+            "9",
+            "9",
+            ("2", "7"),
+        ),
+    ],
+)
+def test_add_string(digit_1, digit_2, carry, expected):
+    result = _add_string(digit_1, digit_2, carry)
+
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "first, second, expected",
+    [
+        (
+            Decimal(number="33.3"),
+            Decimal(number="2.222"),
+            "35.522",
+        ),
+        (
+            Decimal(number="22.222"),
+            Decimal(number="0.0"),
+            "22.222",
+        ),
+        (
+            Decimal(number="12.345"),
+            Decimal(number="67.89"),
+            "80.235",
+        ),
+        (
+            Decimal(number="-12.345"),
+            Decimal(number="67.89"),
+            "55.545",
+        ),
+        (
+            Decimal(number="67.89"),
+            Decimal(number="-12.345"),
+            "55.545",
+        ),
+        (
+            Decimal(number="-12.345"),
+            Decimal(number="-67.89"),
+            "-80.235",
+        ),
+    ],
+)
+def test_add(first, second, expected):
+    result = add(first, second)
+
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "digit_1, digit_2, borrow, returned, expected",
+    [
+        (
+            "3",
+            "2",
+            "0",
+            "0",
+            ("1", "0"),
+        ),
+        (
+            "4",
+            "2",
+            "0",
+            "1",
+            ("1", "0"),
+        ),
+        (
+            "2",
+            "3",
+            "1",
+            "0",
+            ("9", "1"),
+        ),
+        (
+            "2",
+            "3",
+            "1",
+            "1",
+            ("8", "1"),
+        ),
+    ],
+)
+def test_sub_string(digit_1, digit_2, borrow, returned, expected):
+    result, next_returned = _sub_string(digit_1, digit_2, borrow, returned)
+
+    assert (result, next_returned) == expected
+
+
+@pytest.mark.parametrize(
+    "first, second, expected",
+    [
+        (
+            Decimal(number="33.3"),
+            Decimal(number="2.222"),
+            "31.078",
+        ),
+        (
+            Decimal(number="22.222"),
+            Decimal(number="0.0"),
+            "22.222",
+        ),
+        (
+            Decimal(number="12.345"),
+            Decimal(number="67.89"),
+            "-55.545",
+        ),
+        (
+            Decimal(number="-12.345"),
+            Decimal(number="67.89"),
+            "-80.235",
+        ),
+        (
+            Decimal(number="67.89"),
+            Decimal(number="-12.345"),
+            "80.235",
+        ),
+        (
+            Decimal(number="-12.345"),
+            Decimal(number="-67.89"),
+            "55.545",
+        ),
+    ],
+)
+def test_sub(first, second, expected):
+    result = sub(first, second)
+
+    assert result == expected
--- utils/big_math.py
+++ utils/big_math.py
@@ -4,21 +4,108 @@ from typing import Generator, Tuple
 DECIMAL_LIMIT = int(os.getenv("DECIMAL_LIMIT", "10"))
 
 
-def division(numerator: str, denominator: str) -> str:
-    result, remainder = "", ""
+class Decimal:
+    def __init__(self, **kwargs) -> None:
+        self.number = kwargs.get("number")
+        self.whole = kwargs.get("whole")
+        self.decimal = kwargs.get("decimal")
+        self.is_negative = kwargs.get("is_negative")
+        if self.number:
+            if self.is_negative is None:
+                self.is_negative = self.number[0] == "-"
+            self.number = self.number.lstrip("-")
+            self.update_decimal()
+        elif self.whole and self.decimal:
+            if self.is_negative is None:
+                self.is_negative = self.whole[0] == "-"
+            self.whole = self.whole.lstrip("-")
+            self.update_number()
+        else:
+            self.number = "0.0"
+            self.whole = "0"
+            self.decimal = "0"
+            self.is_negative = False
 
-    for number in _get_numerator(numerator):
-        result, remainder = _generate_result(remainder, number, denominator, result)
+    def __eq__(self, other: object) -> bool:
+        return all(
+            float(getattr(self, attr)) == float(getattr(other, attr))
+            for attr in self.__dict__
+        )
 
-    if not result:
-        result = "0."
-    else:
-        result += "."
+    def update_decimal(self):
+        try:
+            whole = self.number.split(".")[0]
+            self.whole = whole if whole else "0"
+            decimal = self.number.split(".")[1]
+            self.decimal = decimal if decimal else "0"
+        except IndexError:
+            self.whole = self.number
+            self.decimal = "0"
 
-    while len(result.split(".")[1]) < DECIMAL_LIMIT and remainder != "0":
-        result, remainder = _generate_result(remainder, "0", denominator, result)
+    def update_number(self):
+        self.number = f"{self.whole if self.whole else '0'}.{self.decimal if self.decimal else '0'}"
 
-    return result
+    def clean_number(self):
+        cleaned_decimal = self.decimal.rstrip("0")
+        self.decimal = cleaned_decimal if cleaned_decimal else "0"
+        cleaned_whole = self.whole.lstrip("0")
+        self.whole = cleaned_whole if cleaned_whole else "0"
+        self.number = f"{self.whole}.{self.decimal}"
+
+    def match_size(self, other):
+        self.whole = self.whole.zfill(len(other.whole))
+        self.decimal = self.decimal.ljust(len(other.decimal), "0")
+        self.update_number()
+
+    def get_number(self) -> str:
+        return f"{self.is_negative * '-'}{self.whole}.{self.decimal}"
+
+    def get_unsigned_number(self):
+        return self.get_number().lstrip("-")
+
+    def get_whole(self) -> str:
+        return f"{self.is_negative * '-'}{self.whole}"
+
+    def get_decimal(self) -> str:
+        return self.decimal
+
+    def get_negative(self) -> bool:
+        return self.is_negative
+
+
+def division(numerator: Decimal, denominator: Decimal) -> str:
+    result_negative = False
+    if (
+        numerator.is_negative
+        and not denominator.is_negative
+        or not numerator.is_negative
+        and denominator.is_negative
+    ):
+        result_negative = True
+
+    result, remainder = Decimal(), ""
+    _match_number_sizes(numerator, denominator)
+    numerator.number, denominator.number = (
+        numerator.number.replace(".", ""),
+        denominator.number.replace(".", ""),
+    )
+    numerator.update_decimal()
+    denominator.update_decimal()
+
+    for number in _get_numerator(numerator.number):
+        result.whole, remainder = _generate_result(
+            remainder, number, denominator.number, result.whole
+        )
+
+    while len(result.decimal) < DECIMAL_LIMIT and remainder != "0":
+        result.decimal, remainder = _generate_result(
+            remainder, "0", denominator.number, result.decimal
+        )
+
+    result.is_negative = result_negative
+    result.update_number()
+    result.clean_number()
+    return result.get_number()
 
 
 def _get_numerator(numerator: str) -> Generator[str, None, None]:
@@ -36,7 +123,10 @@ def _generate_result(
 
     if _fits(remainder, denominator):
         partial_result, remainder = _calculate_division(remainder, denominator)
-        result += partial_result
+        if result == "0":
+            result = partial_result
+        else:
+            result += partial_result
 
     return result, remainder
 
@@ -48,4 +138,71 @@ def _calculate_division(numerator: str, denominator: str) -> Tuple[str, str]:
 
 
 def _fits(numerator: str, denominator: str) -> bool:
-    return bool(int(numerator) // int(denominator))
+    return bool(float(numerator) // float(denominator))
+
+
+def add(first: Decimal, second: Decimal) -> str:
+    if first.is_negative and not second.is_negative:
+        first.is_negative = False
+        return sub(second, first)
+    elif not first.is_negative and second.is_negative:
+        second.is_negative = False
+        return sub(first, second)
+
+    result_negative = first.is_negative and second.is_negative
+    result_number = ""
+    _match_number_sizes(first, second)
+    carry = "0"
+    for digit_1, digit_2 in zip(reversed(first.number), reversed(second.number)):
+        if digit_1 == ".":
+            result_number = "." + result_number
+            continue
+        carry, partial_result = _add_string(digit_1, digit_2, carry)
+        result_number = partial_result + result_number
+
+    result = Decimal(number=result_number, is_negative=result_negative)
+    result.clean_number()
+    return result.get_number()
+
+
+def _add_string(digit_1: str, digit_2: str, carry: str) -> str:
+    return tuple(
+        digit for digit in str(int(digit_1) + int(digit_2) + int(carry)).zfill(2)
+    )
+
+
+def _match_number_sizes(number_1: Decimal, number_2: Decimal) -> None:
+    number_1.match_size(number_2)
+    number_2.match_size(number_1)
+
+
+def _sub_string(digit_1: str, digit_2: str, borrow: str, returned: str) -> str:
+    return str(int(borrow + digit_1) - int(digit_2) - int(returned)), borrow
+
+
+def sub(first: Decimal, second: Decimal) -> str:
+    if first.is_negative and not second.is_negative:
+        second.is_negative = True
+        return add(second, first)
+    elif not first.is_negative and second.is_negative:
+        second.is_negative = False
+        return add(first, second)
+
+    returned, result_number, result_negative = "0", "", False
+    _match_number_sizes(first, second)
+    if float(first.number) < float(second.number):
+        first, second = second, first
+        if not first.is_negative and not second.is_negative:
+            result_negative = True
+
+    for digit_1, digit_2 in zip(reversed(first.number), reversed(second.number)):
+        if digit_1 == ".":
+            result_number = "." + result_number
+            continue
+        borrow = "1" if int(digit_1) < int(digit_2) + int(returned) else "0"
+        partial_result, returned = _sub_string(digit_1, digit_2, borrow, returned)
+        result_number = partial_result + result_number
+
+    result = Decimal(number=result_number, is_negative=result_negative)
+    result.clean_number()
+    return result.get_number()
